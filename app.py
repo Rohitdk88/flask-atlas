06676f4cd393c9e9b64121a54bafd5d42bed9df2
Rohit Dutkunwar <rdutkunwar@deloitte.com>
2024-06-05 17:58:23 +0530

    synonyms mapping db1

--- app.py
+++ app.py
@@ -9,8 +9,7 @@ app = Flask(__name__)
 
 # MongoDB Atlas connection using environment variable
 client = MongoClient(os.getenv('MONGO_URI'))
-db = client.recruitmentdb2
-
+db = client.recruitmentdb1
 # Set up logging
 import logging
 logging.basicConfig(level=logging.DEBUG)
